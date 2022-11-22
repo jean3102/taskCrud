0e39f0913df7b05c71efde01265f0b113f7a326d
jean3102 <56600646+jean3102@users.noreply.github.com>
2022-11-21 21:43:46 -0500

    Update HomeComponent.jsx
    deploy

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,5 +1,5 @@
 import NavComponent from "./NavComponent.jsx";
-import {Col, Container, Row} from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import ModalTaskComponent from "./ModalTaskComponent.jsx";
 import CurrentTaskComponent from "./CurrentTaskComponent.jsx";
 import CompleteTaskComponent from "./CompleteTaskComponent.jsx";
@@ -8,30 +8,36 @@ const HomeComponent = () => {
 
     return (
         <>
-            <NavComponent/>
+            <NavComponent />
 
-            <Container fluid id="body">
-                <Row className="justify-content-lg-center">
+            <Container fluid>
+                <br /><br />
+                <Row className="justify-content-center">
                     <Col lg={10} md={10} xl={6} sm={12} xs={12}>
                         <Row>
-                            <Col lg={6} md={6} xl={6} sm={12} xs={12} >
-                                <h3>Task Pending</h3>
-                                <br/>
-                                <CurrentTaskComponent/>
+
+                            <Col style={styleCol}><h3>Task Pending</h3>
+                                <CurrentTaskComponent />
                             </Col>
-                            <Col lg={6} md={6} sm={12} xs={12}>
-                                <h3>Task Complete</h3>
-                                <br/>
-                                <CompleteTaskComponent/>
+
+                        </Row>
+                        <br/>
+                        <Row>
+                            <Col style={styleCol}><h3>Task Complete</h3>
+                                <CompleteTaskComponent />
                             </Col>
+                        
                         </Row>
                     </Col>
                 </Row>
 
             </Container>
-            <ModalTaskComponent/>
+            <ModalTaskComponent />
         </>
     )
 }
+
+const styleCol = {with:'100%', overflowY: 'auto', color:'red'}
+
 
 export default HomeComponent
